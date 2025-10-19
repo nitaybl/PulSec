cd07d52713ef7b369de638ea712854f5691d8018
nitaybl <60972707+nitaybl@users.noreply.github.com>
2025-10-19 15:35:37 +0300

    Restored deleted files

---
+++ PulSec.lua
@@ -0,0 +1,9 @@
+-- Master Loader
+local scripts = {
+    [8657806289] = "https://raw.githubusercontent.com/nitaybl/PulSec/refs/heads/main/dummiesvsnoobs.lua",
+    [18687417158] = "https://raw.githubusercontent.com/nitaybl/PulSec/refs/heads/main/pulsesaken2.lua",
+    [13559635034] = "https://raw.githubusercontent.com/nitaybl/PulSec/refs/heads/main/combatinitiation.lua"
+}
+local id = game.PlaceId
+local url = scripts[id]
+loadstring(game:HttpGet(url))()
