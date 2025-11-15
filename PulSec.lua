25f7e327c76124b4e1ad363493d2bdd512d403bc
sapheriaxplicitt <sapheria@saph.cc>
2025-11-15 22:35:08 +0900

    Update PulSec.lua

--- PulSec.lua
+++ PulSec.lua
@@ -1,6 +1,6 @@
 -- Master Loader
 local scripts = {
-    [18687417158] = "https://raw.githubusercontent.com/nitaybl/Thisrepoisdefinetleynotgonnabeleaked/pulsesakenv2.lua",
+    [18687417158] = "https://raw.githubusercontent.com/nitaybl/Thisrepoisdefinetleynotgonnabeleaked/pulsesаkenv2.lua",
     [13559635034] = "https://raw.githubusercontent.com/nitaybl/PulSec/refs/heads/main/combatinitiatiоn.lua"
 }
 local id = game.PlaceId
